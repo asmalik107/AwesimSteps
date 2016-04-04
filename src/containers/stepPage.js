d9795287e38903e08a44e2cb38c22d17b6645966
asmalik107 <asmalik@outlook.com>
2016-04-04 17:23:03 +0100

    Add react art

--- src/containers/stepPage.js
+++ src/containers/stepPage.js
@@ -8,23 +8,71 @@ import React, {
 } from 'react-native';
 
 
+import {AnimatedCircularProgress} from 'react-native-circular-progress';
+
 class StepPage extends Component {
     constructor() {
         super();
+
+        this.state = {
+            fill: 'stuff'
+        }
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.today}>
+                    <AnimatedCircularProgress
+                        size={200}
+                        width={10}
+                        fill={90}
+                        tintColor="#00e0ff"
+                        backgroundColor="#3d5875"
+                        rotation={360}
+                    >
+                        {
+                            (fill) => (
+                                <Text style={styles.points}>
+                                    { this.state.fill }
+                                </Text>
+                            )
+                        }
+                    </AnimatedCircularProgress>
+
+
                     <Text>
                         Step
                     </Text>
                 </View>
                 <View style={styles.weekly}>
-                    <Text>
-                        Step
-                    </Text>
+                    <View style={styles.day}>
+                        <Text>
+                            M
+                        </Text>
+                        <AnimatedCircularProgress
+                            size={30}
+                            width={5}
+                            fill={90}
+                            tintColor="#00e0ff"
+                            backgroundColor="#3d5875"
+                            rotation={360}
+                        />
+                    </View>
+                    <View style={styles.day}>
+                        <Text>
+                            T
+                        </Text>
+                        <AnimatedCircularProgress
+                            size={30}
+                            width={5}
+                            fill={90}
+                            tintColor="#00e0ff"
+                            backgroundColor="#3d5875"
+                            rotation={360}
+                        />
+                    </View>
+
                 </View>
             </View>
         );
@@ -38,15 +86,34 @@ var styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
-    }, today: {
+    },
+    today: {
         flex: .5,
         alignItems: 'center',
         justifyContent: 'center'
     },
-    weekly:{
+    weekly: {
         flex: .5,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+    points: {
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        top: 90,
+        left: 50,
+        width: 90,
+        fontSize: 30,
+        textAlign: 'center',
+        color: '#7591af',
+        fontWeight: "100"
+    },
+    day: {
+        flex:1
+    },
+    dayPoints: {
+
     }
 });
 
