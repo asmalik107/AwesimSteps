5715f0ce69b1a6d634fafbb23047fdd313597026
asmalik107 <asmalik@outlook.com>
2016-04-18 15:45:39 +0100

    Create Fit Services for ios and android

--- src/services/fitService.android.js
+++ src/services/fitService.android.js
@@ -1,62 +1,53 @@
 import {NativeModules} from 'react-native';
 const googleFit = NativeModules.RNGoogleFit;
 
-import { DeviceEventEmitter } from 'react-native';
+import {DeviceEventEmitter} from 'react-native';
 
 import moment from 'moment';
 
 
-
-class GoogleFit{
+class GoogleFit {
     constructor() {
-        this.subscriptions = [];
     }
 
     authorize() {
         googleFit.authorize();
     }
 
-    getWeeklySteps(startDate){
+    getWeeklySteps(startDate) {
         //googleFit.getWeeklySteps(startDate.toDate().getTime(), moment().toDate().getTime(), anchorDate.toDate().getTime(), callback);
         googleFit.getWeeklySteps(startDate.toDate().getTime(), moment().toDate().getTime());
     }
 
     observeSteps(callback) {
-        this.subscriptions.push(DeviceEventEmitter.addListener(
+        DeviceEventEmitter.addListener(
             'StepChangedEvent',
             (steps) => callback(steps)
             //(steps) => console.log('StepChangedEvent', steps)
-        ));
+        );
 
         googleFit.observeSteps();
     }
 
     observeHistory(callback) {
-        this.subscriptions.push(DeviceEventEmitter.addListener(
+        DeviceEventEmitter.addListener(
             'StepHistoryChangedEvent',
             (steps) => callback(steps)
             //(steps) => console.log('StepChangedEvent', steps)
-        ));
+        );
     }
 
     onAuthorize(callback) {
-        this.subscriptions.push(DeviceEventEmitter.addListener(
+        DeviceEventEmitter.addListener(
             'AuthorizeEvent',
             (authorized) => callback(authorized)
             //(steps) => console.log('StepChangedEvent', steps)
-        ));
+        );
     }
 
 
     usubscribeListeners() {
-        if(!this.subscriptions.isEmpty()){
-         /*   subscriptions.forEach(function(subscription){
-                subscription.remove();
-            });*/
-            DeviceEventEmitter.removeAllListeners();
-            this.subscriptions = [];
-        }
-
+        DeviceEventEmitter.removeAllListeners();
     }
 
 }
